bbc3760bc4d2dc795e1c498ac69d9bfb2c41e2a3
Alissa Champigny <achampigny4@gmail.com>
2020-12-04 10:14:31 -0500

    removed unused code

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-// import { Navbar, Container, Nav, NavItem } from 'react-bootstrap';
 
 class MyFooter extends Component {
   render() {
     return (
       <nav className="navbar fixed-bottom navbar-light bg-light justify-content-center">
         <ul className="nav justify-content-center">
-          {/* <li className="nav-item">
-            <a className="nav-link text-info" href="./contact.html"><i className="fas fa-address-book" />
-            </a>
-          </li> */}
           <li className="nav-item">
             <a className="nav-link text-info" href="mailto:achampigny4@gmail.com"><i className="fas fa-envelope" /></a>
           </li>
@@ -17,10 +12,10 @@ class MyFooter extends Component {
             <a className="nav-link text-info" href="tel:1-603-793-2449"><i className="fas fa-phone" /></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link text-info" href="https://github.com/achampigny4" target="_blank"><i className="fab fa-github" /></a>
+            <a className="nav-link text-info" href="https://github.com/achampigny4"><i className="fab fa-github" /></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link text-info" href="https://www.linkedin.com/in/alissa-champigny-b1171a1a5/" target="_blank"><i className="fab fa-linkedin" /></a>
+            <a className="nav-link text-info" href="https://www.linkedin.com/in/alissa-champigny-b1171a1a5/"><i className="fab fa-linkedin" /></a>
           </li>
           <div className="row container d-flex flex-column">
             <ul className="d-sm-flex d-block flex-nonwrap text-capitalize text-info">
