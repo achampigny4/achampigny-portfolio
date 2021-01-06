f34560bfe175ea49c08b703e41bdef2f03e275fe
Alissa Champigny <achampigny4@gmail.com>
2021-01-06 12:09:46 -0500

    card height set, links to deployed apps added

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -3,7 +3,7 @@ import './portfolio.css';
 import { CardGroup, Card } from 'react-bootstrap';
 import { Container, Row, Col } from 'react-bootstrap';
 
-class About extends Component {
+class Portfolio extends Component {
     render() {
         return (
             <div>
@@ -15,8 +15,11 @@ class About extends Component {
                     <form className="p-4 mb-4 bg-light text-dark border rounded border-info">
                         <div className="responsive">
                             <div className="shadow p-4 mb-4 bg-white rounded">
-                                {/* card title */}
-                                <h1 className="display-4 text-info">Portfolio</h1>
+                                {/* header */}
+                                <h2 className="text-info">
+                                    <img className="waveIcon" alt="wave" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/icon.png" />
+                                Portfolio
+                                </h2>
                                 {/* divider under title */}
                                 <div className="dropdown-divider" />
 
@@ -25,7 +28,9 @@ class About extends Component {
                                         <CardGroup>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '350px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/jobTrackr.png" alt="Job Trackr login page" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">Job Trackr</h5></Card.Title>
@@ -34,13 +39,16 @@ class About extends Component {
                                                         </Card.Text>
                                                         <div className="card-img-overlay d-flex align-items-end justify-content-center">
                                                             <a className="text-info" href="https://github.com/dylanbest15/Job-Trackr/tree/Alissa"><i className="fab fa-github fa-lg" /></a>
+                                                            <a className="text-info ml-2" href="https://whispering-woodland-82554.herokuapp.com/"><i class="fas fa-external-link-alt fa-lg" /></a>
                                                         </div>
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '350px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/weWorkout.png" alt="We Workout Application" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">We Work Out App</h5></Card.Title>
@@ -49,13 +57,16 @@ class About extends Component {
                                                         </Card.Text>
                                                         <div className="card-img-overlay d-flex align-items-end justify-content-center">
                                                             <a className="text-info" href="https://github.com/achampigny4/we-workout"><i className="fab fa-github fa-lg" /></a>
+                                                            <a className="text-info ml-2" href="https://weworkoutapp.herokuapp.com/"><i class="fas fa-external-link-alt fa-lg" /></a>
                                                         </div>
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '350px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/adultsOnly.png" alt="Adults Only Petfinder website" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">Adults Only Petfinder</h5></Card.Title>
@@ -64,13 +75,16 @@ class About extends Component {
                                                         </Card.Text>
                                                         <div className="card-img-overlay d-flex align-items-end justify-content-center">
                                                             <a className="text-info" href="https://github.com/achampigny4/adults-pet-finder"><i className="fab fa-github fa-lg" /></a>
+                                                            <a className="text-info ml-2" href="https://hilbug.github.io/adults-pet-finder/"><i class="fas fa-external-link-alt fa-lg" /></a>
                                                         </div>
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '350px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/resume.png" alt="Resume" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">Resume</h5></Card.Title>
@@ -79,13 +93,16 @@ class About extends Component {
                                                         </Card.Text>
                                                         <div className="card-img-overlay d-flex align-items-end justify-content-center">
                                                             <a className="text-info" href="https://docs.google.com/document/d/e/2PACX-1vQuGFR0s8Ukhg5mXMot1g7kVOPsa-A_uwF-uxsXzzbpOi7pDotXiteHsDoEOIFi4V6xFAJKzi30EHQi/pub"><i className="fas fa-file-alt fa-lg" /></a>
+                                                            {/* <a className="text-info" href="/AlissaChampignyResume.pdf" download="AlissaChampignyResume"><i class="fas fa-download fa-lg"/></a> */}
                                                         </div>
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '350px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/burger.png" alt="Burgers and Brew Application" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">Burgers and Brew</h5></Card.Title>
@@ -94,13 +111,16 @@ class About extends Component {
                                                         </Card.Text>
                                                         <div className="card-img-overlay d-flex align-items-end justify-content-center">
                                                             <a className="text-info" href="https://github.com/achampigny4/burger"><i className="fab fa-github fa-lg" /></a>
+                                                            <a className="text-info ml-2" href="https://burgers-and-brew.herokuapp.com/"><i class="fas fa-external-link-alt fa-lg" /></a>
                                                         </div>
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '350px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/workdayscheduler.png" alt="screenshot of workday scheduler" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">Work Day Scheduler</h5></Card.Title>
@@ -109,13 +129,16 @@ class About extends Component {
                                                         </Card.Text>
                                                         <div className="card-img-overlay d-flex align-items-end justify-content-center">
                                                             <a className="text-info" href="https://github.com/achampigny4/WorkDayScheduler"><i className="fab fa-github fa-lg" /></a>
+                                                            <a className="text-info ml-2" href="https://achampigny4.github.io/WorkDayScheduler/"><i class="fas fa-external-link-alt fa-lg" /></a>
                                                         </div>
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '410px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/budgettracker.png" alt="Budget tracker app" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">Progressive Budget Tracker</h5></Card.Title>
@@ -124,13 +147,16 @@ class About extends Component {
                                                         </Card.Text>
                                                         <div className="card-img-overlay d-flex align-items-end justify-content-center">
                                                             <a className="text-info" href="https://github.com/achampigny4/budget-tracker"><i className="fab fa-github fa-lg" /></a>
+                                                            <a className="text-info ml-2" href="https://travel-budget-tracker.herokuapp.com/"><i class="fas fa-external-link-alt fa-lg" /></a>
                                                         </div>
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '410px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/teamGenerator.png" alt="command prompt screen shot" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">Team Generator</h5></Card.Title>
@@ -145,7 +171,9 @@ class About extends Component {
                                             </Col>
 
                                             <Col lg={4}>
-                                                <Card>
+                                                <Card
+                                                    style={{ height: '410px' }}
+                                                    className="mb-2">
                                                     <Card.Img variant="top" src="https://raw.githubusercontent.com/achampigny4/achampigny-portfolio/main/public/images/readmegenerator.png" alt="command prompt screen shot" />
                                                     <Card.Body>
                                                         <Card.Title><h5 className="text-info">Readme.md Generator</h5></Card.Title>
@@ -177,4 +205,4 @@ class About extends Component {
     }
 }
 
-export default About;
+export default Portfolio;
