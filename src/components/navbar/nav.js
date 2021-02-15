3e33d116cd10e98d1f1342eb30225f6ff737c635
Alissa Champigny <achampigny4@gmail.com>
2021-02-15 15:00:45 -0500

    mobile responsive collapsable nav bar bootstap

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 // import Name from '../name/name';
 
 class MyNav extends Component {
   render() {
     return (
-      <Navbar className="navbar bg-info m-0 py-0 px-0 px-md-5">
-        <Container>
-          <Navbar.Brand className="m-0 p-3 col-12 col-md-4">
-            <h1 className="display-5"><strong>Alissa Champigny</strong></h1>
-          {/* <Name /> */}
-          </Navbar.Brand>
-          <ul className="nav justify-content-center justify-content-md-end">
-            <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/">About</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/Portfolio">Portfolio</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/Contact">Contact</NavLink>
-            </li>
-          </ul>
-        </Container>
+      <Navbar bg="info" expand="lg">
+        <Navbar.Brand href="/">
+          <h1><strong>Alissa Champigny</strong></h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <NavLink className="nav-link text-light" to="/">About</NavLink>
+            <NavLink className="nav-link text-light" to="/Portfolio">Portfolio</NavLink>
+            <NavLink className="nav-link text-light" to="/Contact">Contact</NavLink>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
