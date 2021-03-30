2bdf6e2bbd68c1250e5d915677f08102cd27b62f
Alissa Champigny <achampigny4@gmail.com>
2021-03-30 11:40:18 -0400

    container removed to improve responsive nav

--- src/components/name/name.js
+++ src/components/name/name.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import './name.css';
 import 'animate.css';
 
 class Name extends Component {
     render() {
         return (
-            <div class="container">
-                <h1 class="animate__animated animate__fadeInDown">Alissa Champigny</h1>
-            </div>
+            <h1 className="animate__animated animate__fadeInDown">Alissa Champigny</h1>
         );
     }
 }
