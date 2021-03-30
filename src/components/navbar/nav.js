8a9d3dd453d2043dc3095dd7454a04845426b0df
Alissa Champigny <achampigny4@gmail.com>
2021-03-30 11:26:50 -0400

    name component added for style

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-// import Name from '../name/name';
+import Name from '../name/name';
 
 class MyNav extends Component {
   render() {
     return (
       <Navbar bg="info" expand="lg">
         <Navbar.Brand href="/">
-          <h1><strong>Alissa Champigny</strong></h1>
+          <Name />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
