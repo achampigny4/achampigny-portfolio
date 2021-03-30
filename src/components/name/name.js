3a9fb387179160bd344b5546bc90e9fc37dd551e
Alissa Champigny <achampigny4@gmail.com>
2021-03-30 11:27:31 -0400

    name animation added for Navbar style

---
+++ src/components/name/name.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+// import './name.css';
+import 'animate.css';
+
+class Name extends Component {
+    render() {
+        return (
+            <div class="container">
+                <h1 class="animate__animated animate__fadeInDown">Alissa Champigny</h1>
+            </div>
+        );
+    }
+}
+
+export default Name;
